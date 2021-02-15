b05adc906c9ef7e6cfa3e61cb5e3694673a10883
alseiitov <dossan.alseiit@gmail.com>
2021-02-15 17:16:45 +0600

    add validation for arrays

--- README.md
+++ README.md
@@ -13,8 +13,10 @@ $ go get -u github.com/alseiitov/validator
 | `password`  | `string` must contain at least one lowercase, one uppercase, one number and one symbol                                       |
 | `email`     | `string` must be a valid email address                                                                                       |
 | `username`  | `string` must contain only letters, digits, dots, underscoreы and dashes                                                     |
-| `min=value` | `string` length must be greater than or equal to given value<br>`int` value must be greater than or equal to given value               |
-| `max=value` | `string` length must be less than or equal to given value<br>`int` value must be less than or equal to given value                     |
+| `min=value` | `string` length must be greater than or equal to given value<br>`int` value must be greater than or equal to given value     |
+| `max=value` | `string` length must be less than or equal to given value<br>`int` value must be less than or equal to given value           |
+
+If array have `required` flag it must contain at least one element. Other flags works for elements in array, but not for array itself.
 
 
 ### Example
@@ -24,47 +26,67 @@ package main
 import (
 	"fmt"
 
-	"github.com/alseiitov/validator"
+	"./repos/validator"
 )
 
 type user struct {
-	Username string `validator:"required,username,min=3,max=32"`
-	Password string `validator:"required,password,min=7,max=32"`
-	Email    string `validator:"required,email"`
-	Age      int    `validator:"required,min=18"`
+	Username  string   `validator:"required,username,min=3,max=32"`
+	Password  string   `validator:"required,password,min=7,max=32"`
+	Email     string   `validator:"required,email"`
+	Age       int      `validator:"required,min=18"`
+	Interests []string `validator:"required,min=2, max=32"`
 }
 
 func main() {
 	users := []user{
 		user{
-			Username: "johndoe",
-			Password: "Password123@",
-			Email:    "john_doe@protonmail.com",
-			Age:      18,
+			Username:  "johndoe",
+			Password:  "Password123@",
+			Email:     "john_doe@protonmail.com",
+			Age:       18,
+			Interests: []string{"IT", "Music"},
 		},
 		user{
-			Username: "",
-			Password: "Password123@",
-			Email:    "john_doe@protonmail.com",
-			Age:      18,
+			Username:  "",
+			Password:  "Password123@",
+			Email:     "john_doe@protonmail.com",
+			Age:       18,
+			Interests: []string{"IT", "Music"},
 		},
 		user{
-			Username: "johndoe",
-			Password: "Password@",
-			Email:    "john_doe@protonmail.com",
-			Age:      18,
+			Username:  "johndoe",
+			Password:  "Password@",
+			Email:     "john_doe@protonmail.com",
+			Age:       18,
+			Interests: []string{"IT", "Music"},
 		},
 		user{
-			Username: "johndoe",
-			Password: "Password123@",
-			Email:    "john_doeprotonmail.com",
-			Age:      18,
+			Username:  "johndoe",
+			Password:  "Password123@",
+			Email:     "john_doeprotonmail.com",
+			Age:       18,
+			Interests: []string{"IT", "Music"},
 		},
 		user{
-			Username: "johndoe",
-			Password: "Password123@",
-			Email:    "john_doe@protonmail.com",
-			Age:      17,
+			Username:  "johndoe",
+			Password:  "Password123@",
+			Email:     "john_doe@protonmail.com",
+			Age:       17,
+			Interests: []string{"IT", "Music"},
+		},
+		user{
+			Username:  "johndoe",
+			Password:  "Password123@",
+			Email:     "john_doe@protonmail.com",
+			Age:       18,
+			Interests: []string{},
+		},
+		user{
+			Username:  "johndoe",
+			Password:  "Password123@",
+			Email:     "john_doe@protonmail.com",
+			Age:       18,
+			Interests: []string{"A"},
 		},
 	}
 
@@ -76,6 +98,7 @@ func main() {
 		}
 	}
 }
+
 ```
 
 ```sh
@@ -89,5 +112,9 @@ Password must contain at least one number
 e-mail is invalid
 
 Age value (17) is lower than minimum value (18)
+
+Interests is required
+
+Interests length (1) is lower than minimum length (2)
 
 ```
--- validator.go
+++ validator.go
@@ -12,77 +12,100 @@ import (
 
 func Validate(data interface{}) error {
 	t := reflect.TypeOf(data)
+	structName := t.String()
 
 	for i := 0; i < t.NumField(); i++ {
 		field := t.Field(i)
-		fieldData := reflect.ValueOf(data).Field(i)
+		kind := field.Type.Kind()
+		data := reflect.ValueOf(data).Field(i).Interface()
 
-		flags, ok := field.Tag.Lookup("validator")
+		flagsStr, ok := field.Tag.Lookup("validator")
 		if !ok {
 			continue
 		}
+		flags := parseFlags(flagsStr)
 
-		flags = trimWhiteSpaces(flags)
-		flagsArray := strings.Split(flags, ",")
-
-		for _, flag := range flagsArray {
-			keyValue := strings.Split(flag, "=")
-
-			switch len(keyValue) {
-			case 1:
-				key := keyValue[0]
-				switch key {
-				case "required":
-					if err := checkRequired(fieldData.Interface()); err != nil {
-						return fmt.Errorf("%v %v", field.Name, err.Error())
-					}
-				case "password":
-					if err := checkPassword(fieldData.String()); err != nil {
-						return fmt.Errorf("%v %v", field.Name, err.Error())
-					}
-				case "email":
-					if err := checkEmail(fieldData.String()); err != nil {
-						return fmt.Errorf("%v", err.Error())
-					}
-				case "username":
-					if err := checkUsername(fieldData.String()); err != nil {
-						return fmt.Errorf("%v", err.Error())
-					}
-				default:
-					log.Fatalf("'%v' invalid key at %v", key, t)
+		if kind == reflect.Array || kind == reflect.Slice {
+			s := reflect.ValueOf(data)
+			if hasRequiredFlag(flags) && s.Len() == 0 {
+				return newError(field.Name, "is required")
+			}
+			for i := 0; i < s.Len(); i++ {
+				d := s.Index(i).Interface()
+				err := checkAll(d, flags, field.Name, structName)
+				if err != nil {
+					return err
 				}
-
-			case 2:
-				key, value := keyValue[0], keyValue[1]
-				switch key {
-				case "min":
-					if err := checkMin(fieldData.Interface(), value); err != nil {
-						return fmt.Errorf("%v %v", field.Name, err.Error())
-					}
-				case "max":
-					if err := checkMax(fieldData.Interface(), value); err != nil {
-						return fmt.Errorf("%v %v", field.Name, err.Error())
-					}
-				default:
-					return fmt.Errorf("%v invalid key", key)
-				}
-			default:
-				log.Fatalf("'%v' invalid format at %v", flag, t)
+			}
+		} else {
+			err := checkAll(data, flags, field.Name, structName)
+			if err != nil {
+				return err
 			}
 		}
 	}
 	return nil
 }
 
-func checkRequired(data interface{}) error {
-	switch v := data.(type) {
-	case int:
-		if v == 0 {
-			return errors.New("is required")
+func checkAll(data interface{}, flags []string, fieldName string, structName string) error {
+	for _, flag := range flags {
+		keyValue := strings.Split(flag, "=")
+
+		switch len(keyValue) {
+		case 1:
+			key := keyValue[0]
+			switch key {
+			case "required":
+				if err := checkRequired(data); err != nil {
+					return newError(fieldName, err.Error())
+				}
+			case "password":
+				if err := checkPassword(fmt.Sprintf("%s", data)); err != nil {
+					return newError(fieldName, err.Error())
+				}
+			case "email":
+				if err := checkEmail(fmt.Sprintf("%s", data)); err != nil {
+					return err
+				}
+			case "username":
+				if err := checkUsername(fmt.Sprintf("%s", data)); err != nil {
+					return err
+				}
+			default:
+				log.Fatalf("'%v' invalid key at %v", key, structName)
+			}
+
+		case 2:
+			key, value := keyValue[0], keyValue[1]
+			switch key {
+			case "min":
+				if err := checkMin(data, value); err != nil {
+					return newError(fieldName, err.Error())
+				}
+			case "max":
+				if err := checkMax(data, value); err != nil {
+					return newError(fieldName, err.Error())
+				}
+			default:
+				return fmt.Errorf("%v invalid key", key)
+			}
+		default:
+			log.Fatalf("'%v' invalid format at %v", flag, structName)
 		}
-	case string:
-		if len(strings.TrimSpace(v)) == 0 {
-			return errors.New("is required")
+	}
+	return nil
+}
+
+func checkRequired(data interface{}) error {
+	v := reflect.ValueOf(data)
+	switch v.Kind() {
+	case reflect.Int:
+		if v.Int() == 0 {
+			return newError("is required")
+		}
+	case reflect.String:
+		if len(strings.TrimSpace(v.String())) == 0 {
+			return newError("is required")
 		}
 	}
 	return nil
@@ -95,16 +118,16 @@ func checkPassword(pass string) error {
 	symbolRegex := regexp.MustCompile(`[!@#~$%^&*()+|_]{1}`)
 
 	if !numRegex.MatchString(pass) {
-		return errors.New("must contain at least one number")
+		return newError("must contain at least one number")
 	}
 	if !lowercaseRegex.MatchString(pass) {
-		return errors.New("must contain at least one lowercase letter")
+		return newError("must contain at least one lowercase letter")
 	}
 	if !uppercaseRegex.MatchString(pass) {
-		return errors.New("must contain at least one uppercase letter")
+		return newError("must contain at least one uppercase letter")
 	}
 	if !symbolRegex.MatchString(pass) {
-		return errors.New("must contain at least one symbol\n(!, @, #, ~, $, %, ^, &, *, (, ), +, |, _, )")
+		return newError("must contain at least one symbol\n(!, @, #, ~, $, %, ^, &, *, (, ), +, |, _, )")
 	}
 	return nil
 }
@@ -113,7 +136,7 @@ func checkEmail(email string) error {
 	emailRegex := regexp.MustCompile(`^[\w-\.]+@([\w-]+\.)+[\w-]{2,24}$`)
 
 	if !emailRegex.MatchString(email) {
-		return errors.New("e-mail is invalid")
+		return newError("e-mail is invalid")
 	}
 	return nil
 }
@@ -122,7 +145,7 @@ func checkUsername(username string) error {
 	usernameRegex := regexp.MustCompile(`^[a-zA-Z0-9._-]+$`)
 
 	if !usernameRegex.MatchString(username) {
-		return errors.New("username is invalid")
+		return newError("username is invalid")
 	}
 	return nil
 }
@@ -174,10 +197,29 @@ func parseInt(intStr string) (int, error) {
 	return n, nil
 }
 
+func parseFlags(flags string) []string {
+	flags = trimWhiteSpaces(flags)
+	flagsArray := strings.Split(flags, ",")
+	return flagsArray
+}
+
 func trimWhiteSpaces(s string) string {
 	whiteSpaces := []string{" ", "\t", "\v", "\n"}
 	for _, w := range whiteSpaces {
 		s = strings.ReplaceAll(s, w, "")
 	}
 	return s
+}
+
+func newError(errs ...string) error {
+	return errors.New(strings.Join(errs, " "))
+}
+
+func hasRequiredFlag(flags []string) bool {
+	for _, f := range flags {
+		if f == "required" {
+			return true
+		}
+	}
+	return false
 }
